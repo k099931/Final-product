0641a8c3ff4c6297227943566833991f0db35bbd
k099931 <k099931@kansai-u.ac.jp>
2022-01-31 17:01:45 +0900

    データベース追加途中

--- selectionblog/database/seeds/RubbersTableSeeder.php
+++ selectionblog/database/seeds/RubbersTableSeeder.php
@@ -16,7 +16,7 @@ class RubbersTableSeeder extends Seeder
         \DB::table('rubbers')->insert([
             [
                 'maker' => 'Nittaku',
-                'name' => 'Fastarc G-1',
+                'name' => 'ファスターク G-1',
                 'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/Fastarc+G-1.png',
                 'speed' => 15.00,
                 'spin' => 12.50,
@@ -27,7 +27,7 @@ class RubbersTableSeeder extends Seeder
         
             [
                 'maker' => 'Nittaku',
-                'name' => 'Fastarc C-1',
+                'name' => 'ファスターク C-1',
                 'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/Fastarc+C-1.png',
                 'speed' => 15.25,
                 'spin' => 12.25,
@@ -35,6 +35,304 @@ class RubbersTableSeeder extends Seeder
                 'price' => 6380,
                 'user_id' => 1,
             ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'ファクティブ',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/FACTIVE.png',
+                'speed' => 14.75,
+                'spin' => 11.75,
+                'hardness' => 35.0,
+                'price' => 4400,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'ファスターク P-1',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/Fastarc+P-1.png',
+                'speed' => 15.50,
+                'spin' => 12.25,
+                'hardness' => 37.5,
+                'price' => 6600,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'ファスターク S-1',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/Fastarc+S-1.png',
+                'speed' => 15.50,
+                'spin' => 11.75,
+                'hardness' => 35.0,
+                'price' => 4840,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'フライアット スピン',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/FLYATT+SPIN.png',
+                'speed' => 14.25,
+                'spin' => 12.25,
+                'hardness' => 35.0,
+                'price' => 4180,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'フライアット ハード',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/FLYATT+HARD.png',
+                'speed' => 15.00,
+                'spin' => 11.55,
+                'hardness' => 35.0,
+                'price' => 4180,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'フライアット ソフト',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/FLYATT+SOFT.png',
+                'speed' => 15.00,
+                'spin' => 11.50,
+                'hardness' => 30.0,
+                'price' => 4180,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'ハモンド',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/HAMMOND.png',
+                'speed' => 12.50,
+                'spin' => 10.00,
+                'hardness' => 30.0,
+                'price' => 3520,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'ザルト',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/ZALT.png',
+                'speed' => 13.75,
+                'spin' => 11.50,
+                'hardness' => 30.0,
+                'price' => 4070,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'キョウヒョウプロ3 -TURBO BLUE-',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/%E3%82%AD%E3%83%A7%E3%82%A6%E3%83%92%E3%83%A7%E3%82%A6%E3%83%97%E3%83%AD3+%E3%82%BF%E3%83%BC%E3%83%9C%E3%83%96%E3%83%AB%E3%83%BC.png',
+                'speed' => 14.75,
+                'spin' => 15.00,
+                'hardness' => 35.0,
+                'price' => 6930,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'キョウヒョウプロ3 -TURBO ORANGE-',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/%E3%82%AD%E3%83%A7%E3%82%A6%E3%83%92%E3%83%A7%E3%82%A6%E3%83%97%E3%83%AD3+%E3%82%BF%E3%83%BC%E3%83%9C%E3%82%AA%E3%83%AC%E3%83%B3%E3%82%B8.png',
+                'speed' => 14.75,
+                'spin' => 15.00,
+                'hardness' => 45.0,
+                'price' => 6600,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'キョウヒョウ ネオ3',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/%E3%82%AD%E3%83%A7%E3%82%A6%E3%83%92%E3%83%A7%E3%82%A6%E3%83%8D%E3%82%AA3.png',
+                'speed' => 11.25,
+                'spin' => 15.00,
+                'hardness' => 42.5,
+                'price' => 5280,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'ルーキング',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/RUUKING.png',
+                'speed' => 8.00,
+                'spin' => 9.25,
+                'hardness' => 32.5,
+                'price' => 2750,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'ジャミン',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/jammin.png',
+                'speed' => 8.25,
+                'spin' => 9.00,
+                'hardness' => 35.0,
+                'price' => 2750,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'ディグニクス05',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/Dignics+05.png',
+                'speed' => 13.50,
+                'spin' => 12.00,
+                'hardness' => 35.0,
+                'price' => 0000,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'ディグニクス09C',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/Dignics+09C.png',
+                'speed' => 13.00,
+                'spin' => 13.00,
+                'hardness' => 39.5,
+                'price' => 0000,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'ディグニクス64',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/Dignics+64.png',
+                'speed' => 14.00,
+                'spin' => 11.00,
+                'hardness' => 40.0,
+                'price' => 0000,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'テナジー05',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/TENERGY+05.png',
+                'speed' => 13.00,
+                'spin' => 11.50,
+                'hardness' => 30.5,
+                'price' => 0000,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'テナジー80',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/TENERGY+80.png',
+                'speed' => 13.25,
+                'spin' => 11.25,
+                'hardness' => 30.5,
+                'price' => 0000,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'テナジー64',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/TENERGY+64.png',
+                'speed' => 13.50,
+                'spin' => 10.50,
+                'hardness' => 30.5,
+                'price' => 0000,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'ロゼナ',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/ROZENA.png',
+                'speed' => 13.00,
+                'spin' => 10.80,
+                'hardness' => 30.0,
+                'price' => 5500,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'ブライス ハイスピード',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/BryceHIGHSPEED.png',
+                'speed' => 14.50,
+                'spin' => 10.30,
+                'hardness' => 30.0,
+                'price' => 6600,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'ラウンデル',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/ROUNDELL.png',
+                'speed' => 12.80,
+                'spin' => 10.20,
+                'hardness' => 30.0,
+                'price' => 4620,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'スレイバー',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/SRIVER.png',
+                'speed' => 10.00,
+                'spin' => 8.00,
+                'hardness' => 33.0,
+                'price' => 3520,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'スレイバーEL',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/SRIVER+EL.png',
+                'speed' => 10.00,
+                'spin' => 8.00,
+                'hardness' => 30.0,
+                'price' => 3520,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'アイビス',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/AIBISS.png',
+                'speed' => 10.50,
+                'spin' => 11.00,
+                'hardness' => 45.0,
+                'price' => 5500,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'タキネス ドライブ',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/TACKNESS+DRIVE.png',
+                'speed' => 8.50,
+                'spin' => 9.00,
+                'hardness' => 33.0,
+                'price' => 3080,
+                'user_id' => 1,
+            ],
+            
+            [
+                'maker' => 'BUTTERFLY',
+                'name' => 'タキネス チョップ',
+                'image' =>'https://selectionblog.s3.us-east-2.amazonaws.com/Rubber/TACKNESS+CHOP.png',
+                'speed' => 6.50,
+                'spin' => 10.00,
+                'hardness' => 28.0,
+                'price' => 3080,
+                'user_id' => 1,
+            ],
+            
         ]);
     }
 }
--- selectionblog/database/seeds/RucketsTableSeeder.php
+++ selectionblog/database/seeds/RucketsTableSeeder.php
@@ -15,7 +15,7 @@ class RucketsTableSeeder extends Seeder
         \DB::table('ruckets')->insert([
             [
                 'maker' => 'Nittaku',
-                'name' => 'Viorin',
+                'name' => 'バイオリン',
                 'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/Viorin.png',
                 'repulsion' => 'Midslow',
                 'feeling' => 'Soft',
@@ -25,13 +25,234 @@ class RucketsTableSeeder extends Seeder
         
             [
                 'maker' => 'Nittaku',
-                'name' => 'Acoustic',
+                'name' => 'アコースティック',
                 'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/Acoustic.png',
                 'repulsion' => 'Mid',
                 'feeling' => 'Middle',
                 'weight' => 88.0,
                 'price' => 19800,
             ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'ファクティブ7',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/FACTIVE7.png',
+                'repulsion' => 'Midfirst',
+                'feeling' => 'Hard',
+                'weight' => 83.0,
+                'price' => 5720,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'セプティアーフィール',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/Septear+feel.png',
+                'repulsion' => 'Midfirst',
+                'feeling' => 'Middle',
+                'weight' => 88.0,
+                'price' => 13200,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'バイオリン カーボン',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/Viorin+carbon.png',
+                'repulsion' => 'Midfirst',
+                'feeling' => 'Hard',
+                'weight' => 90.0,
+                'price' => 22000,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'アコースティック カーボン',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/Acoustic+carbon.png',
+                'repulsion' => 'Midfirst',
+                'feeling' => 'Hard',
+                'weight' => 90.0,
+                'price' => 22000,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'フライアット カーボン',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/FLYATT+CARBON.png',
+                'repulsion' => 'Mid',
+                'feeling' => 'Middle',
+                'weight' => 82.0,
+                'price' => 7480,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => '佳純ベーシック',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/Kasumi+Basic.png',
+                'repulsion' => 'Mid',
+                'feeling' => 'Middle',
+                'weight' => 85.0,
+                'price' => 8250,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'ラティカ',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/Latika.png',
+                'repulsion' => 'Mid',
+                'feeling' => 'Middle',
+                'weight' => 88.0,
+                'price' => 7150,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'セプティアー',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/Septear.png',
+                'repulsion' => 'Mid',
+                'feeling' => 'Hard',
+                'weight' => 85.0,
+                'price' => 9680,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'サナリオンS',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/Sanalion.png',
+                'repulsion' => 'Midslow',
+                'feeling' => 'Hard',
+                'weight' => 78.0,
+                'price' => 5500,
+            ],
+            
+            [
+                'maker' => 'Nittaku',
+                'name' => 'ビオンセロ',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/VIOLONCELLO.png',
+                'repulsion' => 'Midslow',
+                'feeling' => 'Soft',
+                'weight' => 92.0,
+                'price' => 19800,
+            ],
+            
+            [
+                'maker' => 'VICTAS',
+                'name' => 'KOKI NIWA WOOD',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/KOKI+NIWA+WOOD.png',
+                'repulsion' => 'First',
+                'feeling' => 'Hard',
+                'weight' => 90.0,
+                'price' => 16500,
+            ],
+            
+            [
+                'maker' => 'VICTAS',
+                'name' => 'KOKI NIWA',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/KOKI+NIWA.png',
+                'repulsion' => 'Midfirst',
+                'feeling' => 'Hard',
+                'weight' => 88.0,
+                'price' => 22000,
+            ],
+            
+            [
+                'maker' => 'VICTAS',
+                'name' => 'QUARTET VFC',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/QUARTET+VFC.png',
+                'repulsion' => 'First',
+                'feeling' => 'Hard',
+                'weight' => 88.0,
+                'price' => 16500,
+            ],
+            
+            [
+                'maker' => 'VICTAS',
+                'name' => 'ZX-GEAR IN',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/ZX-GEAR+IN.png',
+                'repulsion' => 'Midfirst',
+                'feeling' => 'Hard',
+                'weight' => 92.0,
+                'price' => 13200,
+            ],
+            
+            [
+                'maker' => 'VICTAS',
+                'name' => 'ZX-GEAR FIBER',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/ZX-GEAR+FIBER.png',
+                'repulsion' => 'Mid',
+                'feeling' => 'Soft',
+                'weight' => 85.0,
+                'price' => 11000,
+            ],
+            
+            [
+                'maker' => 'VICTAS',
+                'name' => 'Fire Fall AC',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/Fire+Fall+AC.png',
+                'repulsion' => 'First',
+                'feeling' => 'Hard',
+                'weight' => 88.0,
+                'price' => 11000,
+            ],
+            
+            [
+                'maker' => 'VICTAS',
+                'name' => 'DYNA FIVE',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/DYNA+FIVE.png',
+                'repulsion' => 'Midslow',
+                'feeling' => 'Soft',
+                'weight' => 85.0,
+                'price' => 7700,
+            ],
+            
+            [
+                'maker' => 'VICTAS',
+                'name' => 'DYNA CARBON',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/DYNA+CARBON.png',
+                'repulsion' => 'Midslow',
+                'feeling' => 'Middle',
+                'weight' => 85.0,
+                'price' => 8800,
+            ],
+            
+            [
+                'maker' => 'VICTAS',
+                'name' => 'KOJI MATSUSHITA',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/KOJI+MATSUSHITA.png',
+                'repulsion' => 'Slow',
+                'feeling' => 'Soft',
+                'weight' => 87.0,
+                'price' => 11000,
+            ],
+            
+            [
+                'maker' => 'VICTAS',
+                'name' => 'SWAT',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/SWAT.png',
+                'repulsion' => 'Midslow',
+                'feeling' => 'SOft',
+                'weight' => 85.0,
+                'price' => 6050,
+            ],
+            
+            [
+                'maker' => 'VICTAS',
+                'name' => 'SWAT CARBON',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/SWAT+CARBON.png',
+                'repulsion' => 'Midslow',
+                'feeling' => 'Middle',
+                'weight' => 85.0,
+                'price' => 7150,
+            ],
+            
+            [
+                'maker' => 'VICTAS',
+                'name' => 'Liam Pitchford',
+                'image' => 'https://selectionblog.s3.us-east-2.amazonaws.com/Rucket/Liam+Pitchford.png',
+                'repulsion' => 'First',
+                'feeling' => 'Hard',
+                'weight' => 90.0,
+                'price' => 22000,
+            ],
+            
         ]);
     }
 }
